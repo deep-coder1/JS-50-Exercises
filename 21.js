e857efb64147fc181a946166fb453a10e29b1fb5
deep-coder1 <ndktbd@gmail.com>
2024-01-12 21:16:12 +0530

    Exercise 21 Solve

--- 21.js
+++ 21.js
@@ -5,3 +5,27 @@
 // fact = 5! = 5*4*3*2*1
 //        4! = 4*3*2*1
 
+// FontFaceSet(n) = n* n-1 * n-2 .....!
+// n! = n*(n-1)!
+// 5! = 5*4!
+//    = 5*4!
+
+function factorial(n){
+    if(n === 1){
+        return 1;
+    } else{
+        return n*(factorial(n-1))
+    }
+}
+
+console.log(factorial(5));
+
+// Output:-
+// 120
+
+// Ex:
+// 5 * factorial(5-1)
+// 4 * factorial(4-1)
+// 3 * factorial(3-1)
+// 2 * factorial(2-1)
+// 1
