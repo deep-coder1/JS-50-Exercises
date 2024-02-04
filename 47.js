fd76997a45c9ecbb009402fefc6ff821de4daa73
deep-coder1 <ndktbd@gmail.com>
2024-02-04 23:12:27 +0530

    Exercise 47 Add

---
+++ 47.js
@@ -0,0 +1,39 @@
+// Random Exercise*
+// Create a function that takes a string as an argument. The function
+// must return a string containing 1s and 0s based on the string
+// argument's words. Iÿ any word in the argument is not equal to "zero" or
+// "one" (case insensitive), you should ignore it. The returned string's
+// length should be a multiple of 8, if the string is not a multiple of 8 you
+// should remove the numbers in excess.
+// textToNumberBinary("Zero one zero ONE zero one zero one") ➞ "01010101"
+// textToNumberBinary("zero one zero one zero one zero one one two") ➞ ""
+// textToNumberBinary("zero one zero one zero one zero three") ➞ ""
+
+function textToNumberBinary(str){
+    const words = str.split(' ').filter(word=>
+        word.toLowerCase() === "zero" || word.toLowerCase() === "one");
+
+        console.log(words);
+
+        let binaryString = words.map((word)=>{
+            return word.toLowerCase() === "one" ?
+            "1" : "0"
+        }).join('');
+
+        const excessLength = binaryString.length % 8;
+
+        if(excessLength !== 0){
+            binaryString = binaryString.slice(0,0);
+        }
+
+        return binaryString;
+        console.log(binaryString)
+}
+
+// console.log(textToNumberBinary("Zero one zero ONE zero one zero one"));
+// console.log(textToNumberBinary("Zero one one zero ONE zero one zero one"));
+console.log(textToNumberBinary("Zero two one zero ONE zero one zero one"));
+
+// Output:- 
+// (8) ['Zero', 'one', 'zero', 'ONE', 'zero', 'one', 'zero', 'one']
+// 47.js:35 01010101
