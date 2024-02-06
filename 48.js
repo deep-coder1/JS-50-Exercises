600d7c1845980d657040c730e0f0011044bd6fd6
deep-coder1 <ndktbd@gmail.com>
2024-02-06 20:59:27 +0530

    Exercise 48 Add

---
+++ 48.js
@@ -0,0 +1,44 @@
+// Random Exercise*
+// Create a function that takes in a sentence and returns a
+// running list of consonants per word and vowels per word.
+// stringCode("Happy Birthday To Me!")
+// ➞ ["4 6 1 1", "1 2 1 1"]
+// // Consonants - 4 : [H, p, p, y], 6 : [B, r, t, h, d, y], 1: [T], 1 : [M]
+// // Vowels - 1: [a], 2 : [i, a], 1: [o], 1: [e]
+
+function stringCode(sentence){
+
+    function IsConsonant(char){
+        const consonants = "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ";
+        return consonants.includes(char);
+    }
+
+    function countConsonantsAndVowels(word){
+        let consonants = 0;
+        let vowels = 0;
+
+        for(const char of word){
+            if(IsConsonant(char)){
+                consonants++;
+            }
+            else if("aeiouAEIOU".includes(char)){
+                vowels++;
+            }
+        }
+
+        return consonants + " " +vowels;
+    }
+
+    const words = sentence.split(' ');
+    console.log(words);
+
+    const consonantCount = words.map(word => countConsonantsAndVowels(word));
+
+    return consonantCount;
+}
+
+console.log(stringCode("Happy Birthday To Me"));
+
+// Output:-
+// (4) ['Happy', 'Birthday', 'To', 'Me']
+// 48.js:40 (4) ['4 1', '6 2', '1 1', '1 1']
